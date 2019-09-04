39184891598e871c17ceb1b0dbe43b340d9b49cd
Valery Kokanov <valery.kokanov@ooma.com>
2019-09-04 20:35:44 +0300

    Initial Commit

---
+++ LastFMClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LastFMClient
+//
+//  Created by Valery Kokanov on 04/09/2019.
+//  Copyright © 2019 my-night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
